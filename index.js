33a1af629257ebe3e678608f11ad80efaba9155e
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-04-15 19:20:17 +0500

    running

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
+
 const path = require("path");
 const serverless = require("serverless-http");
 const connectToMongo = require("./db");
 
 const app = express();
-const port = process.env.SERVER_PORT || 5000;
-const appOrigin = process.env.REACT_APP_API_ORIGIN;
+const port = 5001;
+const appOrigin = "https://vercel-frontend1-gray.vercel.app/";
 
 // Connect to MongoDB
 connectToMongo();
@@ -27,12 +26,8 @@ app.use(cors(corsOptions));
 
 // Middlewares
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
+
+
 
 // Routes
 app.get("/", (req, res) => {
@@ -43,16 +38,7 @@ app.use("/api/nft", require("./routes/nft"));
 app.use("/api/cart", require("./routes/cart"));
 app.use("/api/chat", require("./routes/chat"));
 
-// Static file serving
-app.use(express.static(path.join(__dirname, "build")));
-app.use(
-  "/images",
-  (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", appOrigin);
-    next();
-  },
-  express.static(path.join(__dirname, "public/images"))
-);
+
 
 // For local testing only (not used by serverless)
 if (process.env.NODE_ENV !== "production") {
