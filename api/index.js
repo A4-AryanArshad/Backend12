11f44b015b4ec9ddd8183e0bdeb512f67ba317bb
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-04-15 21:43:05 +0500

    new

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const connectToMongo = require('./db');
+const connectToMongo = require('../db');
 const express = require('express');
 const cors = require('cors');
 
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 // Existing routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/nft', require('./routes/nft'));
-app.use('/api/cart', require('./routes/cart'));
+app.use('/api/auth', require('../routes/auth'));
+app.use('/api/nft', require('../routes/nft'));
+app.use('/api/cart', require('../routes/cart'));
 
-app.use('/api/chat', require('./routes/chat'));
+app.use('/api/chat', require('../routes/chat'));
 
 app.listen(port, () => {
     console.log(`NFT Authentication Backend Listening at http://localhost:${port}`);
